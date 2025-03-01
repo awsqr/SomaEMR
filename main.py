3c08079b32f44205942a2047e79a1a4107b636bd
awsqr <100445429+awsqr@users.noreply.github.com>
2025-03-01 18:58:07 -0500

    add login page

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
-import requests.exceptions
-from fastapi import FastAPI
-from requests import HTTPError
+from http import HTTPStatus
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-db = {7: 'healthy'}
+db = {7: 'healthy',
+      12: 'sick',
+      17: 'healthy'}
+
+app.mount('/static', StaticFiles(directory='static'), name='static')
+templates = Jinja2Templates(directory='templates')
+
+
+@app.get('/')
+def read_root(request: Request):
+    return templates.TemplateResponse(
+        'index.html',
+        {'request': request,
+         'title': 'Soma EMR Home'}
+    )
 
 @app.get('/records/{patient_id}')
 def get_record(patient_id: int):
-    return {f'patient number {patient_id}': db.get(patient_id)}
+    return JSONResponse(
+        content={f'patient number {patient_id}': db.get(patient_id)},
+        status_code=HTTPStatus.OK
+    )
 
 @app.post('/records/{patient_id}')
 def add_patient(patient_id: int, status: str):
     global db
+    if (patient_id in db):
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST,
+                            detail="Invalid ID")
     db[patient_id] = status
-    return {'patient_id': patient_id, "status": status}
+    return JSONResponse(
+        content={'patient_id': patient_id, "status": status},
+        status_code=HTTPStatus.CREATED
+    )
 
 @app.put('/records/{patient_id}')
 def update_record(patient_id:int, status: str):
@@ -22,5 +47,5 @@ def update_record(patient_id:int, status: str):
         db[patient_id] = status
         return {'patient_id': patient_id, "status": status}
 
-    return requests.exceptions.HTTPError
+    raise HTTPException(status_code=HTTPStatus.NOT_FOUND)
 
